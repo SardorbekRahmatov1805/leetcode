2bee28d182dfa63ae9db182a02e4c6d937e7d5fd
Sardorbek Rahmatov <sardorbekrakhmatov1805@gmail.com>
2026-04-13 06:21:42 +0500

    string_number

--- palindrom.py
+++ palindrom.py
@@ -1,15 +1,15 @@
-'''
-son=int(input("sonni kiriting")) #sonni kiritamiz
-if str(son)==str(son)[::-1]: #olingan sonni  matinga utkazib oxiridan orqaga qaytaradi
-  print("palindrom son")
-else:
-  print("palindrom emas")
-'''
-
 def palindrom(son):
-  if str(son)==str(son)[::-1]:
-    return 'palindrom son'
+  if son<0:
+    return False
+  s=0
+  palindromi=son
+  while son!=0:
+    son1=son%10
+    s=s*10+son1
+    son//=10
+  if s==palindromi:
+    return True
   else:
-    return "palindrom emas"
-print(palindrom(121))
-print(palindrom(12))
+   return False
+son3=int(input("sonni kiriiting:"))
+print(palindrom(son3))
